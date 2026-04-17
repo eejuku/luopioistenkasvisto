ceee9669e494e3c327ff1aca394fb69c3a49e698
Eero Kuitunen <eero.kuitunen@gmail.com>
2026-04-18 00:13:50 +0300

    kartta lightboxiin

--- wp-content/themes/kasvisto/footer.php
+++ wp-content/themes/kasvisto/footer.php
@@ -6,7 +6,41 @@
         <div class="footer-links">
             </div>
     </div>
+
 </footer>
 
-<?php wp_footer(); ?> </body>
+<?php wp_footer(); ?>
+
+<script src="https://cdn.jsdelivr.net/npm/glightbox/dist/js/glightbox.min.js"></script>
+<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/glightbox/dist/css/glightbox.min.css" />
+
+<script>
+(function() {
+    document.addEventListener('click', function(e) {
+        const link = e.target.closest('.karttalinkki');
+        
+        if (link) {
+            e.preventDefault();
+            let mapURL = link.getAttribute('href');
+
+            if (typeof GLightbox !== 'undefined') {
+                const singleLightbox = GLightbox({
+                    // Automaattinen asetusten haku iframe-tyypille
+                    elements: [{
+                        href: mapURL,
+                        type: 'iframe', // Pakotetaan iframe
+                        source: 'local', // Joissain versioissa auttaa ohittamaan tunnistusongelmat
+                        width: '90vw',
+                        height: '85vh'
+                    }]
+                });
+                singleLightbox.open();
+            } else {
+                window.open(mapURL, '_blank');
+            }
+        }
+    }, false);
+})();
+</script>
+</body>
 </html>
