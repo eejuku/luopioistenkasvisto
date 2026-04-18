0bbff08985763ab757295385be6bfc78be5717d5
Eero Kuitunen <eero.kuitunen@gmail.com>
2026-04-18 11:30:00 +0300

    kartan lightboxin toimivuuden virittelyä

--- wp-content/themes/kasvisto/footer.php
+++ wp-content/themes/kasvisto/footer.php
@@ -11,6 +11,7 @@
 
 <?php wp_footer(); ?>
 
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css">
 <script src="https://cdn.jsdelivr.net/npm/glightbox/dist/js/glightbox.min.js"></script>
 <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/glightbox/dist/css/glightbox.min.css" />
 
