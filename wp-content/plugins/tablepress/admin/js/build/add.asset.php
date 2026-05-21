2109561d761aa4408034fa0252b0e27035fa8c5f
Eero Kuitunen <eero.kuitunen@gmail.com>
2026-05-21 20:24:47 +0300

    Poistettu .DS_Store-tiedostot

--- wp-content/plugins/tablepress/admin/js/build/add.asset.php
+++
@@ -1,1 +0,0 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'react-jsx-runtime', 'wp-components', 'wp-dom-ready', 'wp-hooks', 'wp-i18n'), 'version' => '5e48ec2c8e5f50cba886');
--- wp-content/plugins/tablepress/admin/js/build/import.asset.php
+++
@@ -1,1 +0,0 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'react-jsx-runtime', 'wp-components', 'wp-dom-ready', 'wp-hooks', 'wp-i18n', 'wp-primitives'), 'version' => '23b76a544861f2c5cc9e');
